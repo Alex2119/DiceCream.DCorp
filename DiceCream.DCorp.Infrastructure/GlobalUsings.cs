ff29c0e500a757573ac60d82032142945ab6376b
Alexandre PEROT <alexandre.perot@web-atrio.com>
2024-11-06 23:13:23 +0100

    Ajout de références de projet et nettoyage mineur
    Ajout de références de projet dans les fichiers .csproj pour permettre les dépendances nécessaires :
    - `DiceCream.DCorp.Domain` dans `DiceCream.DCorp.Application.DTO.csproj`
    - `DiceCream.DCorp.Application.DTO` et `DiceCream.DCorp.Domain` dans `DiceCream.DCorp.Application.Lib.csproj`
    
    Suppression d'une ligne vide dans `GlobalUsings.cs` pour améliorer la lisibilité.

--- DiceCream.DCorp.Infrastructure/GlobalUsings.cs
+++ DiceCream.DCorp.Infrastructure/GlobalUsings.cs
@@ -3,4 +3,4 @@ global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Design;
 global using Microsoft.Extensions.Configuration;
-
+ 
